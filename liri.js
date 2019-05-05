9b3b988b8106f0d914c584671a556e878ceadfee
AMDLnghrns <aduncan001@gmail.com>
2019-05-05 09:25:26 -0500

    post backing out update that did not work

--- liri.js
+++ liri.js
@@ -97,6 +97,38 @@ function concert() {
     });
 }
 
+function spotify() {
+  require("dotenv").config();
+  var keys = require("./keys");
+  var Spotify = require("node-spotify-api");
+  var spotify = new Spotify(keys.spotify);
+
+  var queryType = "track";
+  var limitValue = 1;
+  var queryValue = ifSpotifyBlank();
+
+  function ifSpotifyBlank() {
+    if (process.argv.length < 4) {
+      return "The Sign Ace of Base";
+    } else {
+      return process.argv[3];
+    }
+  }
+
+  spotify.search(
+    { type: queryType, query: queryValue, limit: limitValue },
+    function(err, data) {
+      if (err) {
+        return console.log("Error occurred: " + err);
+      }
+      console.log("Artist: " + data.tracks.items[0].artists[0].name);
+      console.log("Song: " + data.tracks.items[0].name);
+      console.log("Link: " + data.tracks.items[0].external_urls.spotify);
+      console.log("Album: " + data.tracks.items[0].album.name);
+    }
+  );
+}
+
 function random() {
   var fs = require("fs");
   fs.readFile("random.txt", "utf8", function(error, data) {

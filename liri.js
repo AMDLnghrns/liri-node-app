e657189c64bc9f9d798e5fb4670ad21ce72e3793
AMDLnghrns <aduncan001@gmail.com>
2019-05-05 13:41:32 -0500

    Added screenshots for testing, renamed them, and added pseudocode for the last part

--- liri.js
+++ liri.js
@@ -5,13 +5,12 @@ if (process.argv[2] == 'movie') {
 } else if (process.argv[2] == 'concert') {
   concert();
 } else {
-  
+  error();
 }
 
 function movie() {
   var axios = require("axios");
   var movie = ifMovieBlank();
-
   function ifMovieBlank() {
     if (process.argv.length < 4) {
       return "Mr. Nobody";
@@ -25,8 +24,8 @@ function movie() {
     .then(function(response) {
       console.log("Title: " + response.data.Title);
       console.log("Year: " + response.data.Year);
-      console.log("IMDB: " + response.data.Ratings);
-      console.log("Rotten Tomatoes: " + response.data.Ratings);
+      console.log("IMDB: " + response.data.imdbRating);
+      console.log("Rotten Tomatoes: " + response.data.Ratings[1].Value);
       console.log("Country: " + response.data.Country);
       console.log("Language: " + response.data.Language);
       console.log("Plot: " + response.data.Plot);
@@ -68,6 +67,7 @@ function spotify() {
 
 function concert() {
   var axios = require("axios");
+  var moment = require("moment");
   var artist = ifArtistBlank();
   // var artist = 'Phantogram';
 
@@ -93,51 +93,33 @@ function concert() {
           ", " +
           response.data[0].venue.region
       );
-      console.log("Date: " + response.data[0].datetime); // Date of the Event (use moment to format this as "MM/DD/YYYY")
+      console.log("Date: " + moment(response.data[0].datetime).format('L')); // Date of the Event (use moment to format this as "MM/DD/YYYY")
     });
 }
 
-function spotify() {
-  require("dotenv").config();
-  var keys = require("./keys");
-  var Spotify = require("node-spotify-api");
-  var spotify = new Spotify(keys.spotify);
-
-  var queryType = "track";
-  var limitValue = 1;
-  var queryValue = ifSpotifyBlank();
-
-  function ifSpotifyBlank() {
-    if (process.argv.length < 4) {
-      return "The Sign Ace of Base";
-    } else {
-      return process.argv[3];
-    }
-  }
-
-  spotify.search(
-    { type: queryType, query: queryValue, limit: limitValue },
-    function(err, data) {
-      if (err) {
-        return console.log("Error occurred: " + err);
-      }
-      console.log("Artist: " + data.tracks.items[0].artists[0].name);
-      console.log("Song: " + data.tracks.items[0].name);
-      console.log("Link: " + data.tracks.items[0].external_urls.spotify);
-      console.log("Album: " + data.tracks.items[0].album.name);
-    }
-  );
-}
-
-function random() {
+function error() {
   var fs = require("fs");
-  fs.readFile("random.txt", "utf8", function(error, data) {
-    if (error) {
-      return console.log(error);
-    }
-    return data.slice(8);
-    // console.log(data.slice(8));
-    // var dataArr = data.split(",");
-    // console.log(dataArr);
+  fs.readFile("random.txt", "utf8", function (error, data) {
+      if (error) {
+          return console.log(error);
+      }
+      var dataArr = data.split(",");
+      var dataArr2 = dataArr[0].split(' ')
+      appSelector = dataArr2[0];
+      var dataArr3 = dataArr[0].split('"');
+      console.log(dataArr3[1]);
+      input = dataArr[0];
+
+      if (appSelector == "spotify") {
+          spotify(); //run the dataArr3[1] value through the spotify function
+      }
+      else if (appSelector == "concert") {
+          concerts();
+      }
+      else if (appSelector == "movie") {
+          movie();
+      }
+
   });
 }
+
